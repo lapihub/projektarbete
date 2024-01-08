45ba1ef060d2d186da1d1635bf8a2ea9a96eec81
Lara Pirdaoud <lara.pirdaoud@hotmail.com>
2024-01-08 13:33:12 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Box from './components/Box.jsx'
+
+function App() {
+  return (
+    <div>
+      <p>hej</p>
+      <Box />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Box.jsx
@@ -0,0 +1,11 @@
+import './box.css'
+
+function Box() {
+    return (
+        <div className="box">
+            box
+        </div>
+    )
+}
+
+export default Box
