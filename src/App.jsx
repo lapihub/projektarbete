a3c5be318b6ca707fe5ba7e7eb4edaa1dbff36dc
Lara Pirdaoud <lara.pirdaoud@hotmail.com>
2024-01-08 14:38:21 +0100

    create button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Box from './components/Box.jsx'
+import Button from './components/Button.jsx'
 
 function App() {
   return (
     <div>
       <p>hej</p>
       <Box />
+      <Button />
     </div>
   )
 }
---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+function Button() {
+    return (
+        <button>knapp</button>
+    )
+}
+
+export default Button
